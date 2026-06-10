4ac47efc9223f862af355fd8604d9b0f089dd5a0
Subhasmita Bal <subhasmitabal02@gmail.com>
2026-06-10 15:37:22 +0530

    Found all words with vowels.

--- file_reading_practice/words_with_all_vowels.py
+++ file_reading_practice/words_with_all_vowels.py
@@ -40,3 +40,17 @@ Explanation:
 =================================================
 
 """
+vowels = {'a', 'e', 'i', 'o', 'u'}
+count = 0
+
+with open("sowpods.txt", "r") as file:
+    for word in file:
+        word = word[:-1].lower()   
+
+        letters = set(word)
+
+        if vowels.issubset(letters):
+            print(word)
+            count += 1
+
+print("Total words with all vowels:", count)
